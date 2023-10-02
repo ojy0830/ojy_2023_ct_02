e908924ee1053b17e1ee54a418f1660c3cee52de
ojy0830 <139185480+ojy0830@users.noreply.github.com>
2023-10-03 00:19:44 +0900

    졸업프로젝트 마무리

---
+++ src/subject1/script/reference.js
@@ -0,0 +1,24 @@
+const reference = [
+  {
+    author: "지니더하기지니",
+    title: "강릉 가볼만한 곳, 주문진 영진해변 도깨비 드라마촬영지",
+    year: "2021",
+    sourceTitle: "",
+    link: "https://2genie.tistory.com/300",
+  },
+  {
+    author: "한국일보",
+    title: "[HI★초점] '그 해 우리는', 기대에 부응 못한 초라한 퇴장",
+    year: "2022",
+    sourceTitle: "",
+    link: "https://m.hankookilbo.com/News/Read/A2022012514590005477",
+  },
+  {
+    author: "아시아프레스",
+    title:
+      "드디어 ‘사랑의 불시착’이 어둠 속에서 확산되기 시작했다! ‘목숨을 걸고’ 본 여성들 인터뷰",
+    year: "2021",
+    sourceTitle: "",
+    link: "https://www.asiapress.org/korean/2021/04/society-human-rights/tv-drama/",
+  },
+];
---
+++ src/subject2/script/reference.js
@@ -0,0 +1,9 @@
+const reference = [
+  {
+    author: "문화재청 궁능유적본부 경복궁관리소 ",
+    title: "경복궁의 사계",
+    year: "2014",
+    sourceTitle: "",
+    link: "https://www.royalpalace.go.kr/content/preview/preview03_02.asp",
+  },
+];
